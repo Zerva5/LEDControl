40890b5731b678a9f953f278261c03f86fdfe3e1
Zerva5 <lucasmmayall@gmail.com>
2019-05-07 19:26:38 -0700

    GUI is functional, not done but functional

--- main.py
+++ main.py
@@ -24,7 +24,7 @@ colorDict = {
 }
 
 
-Serial = serial.Serial("/dev/ttyACM0")
+# Serial = serial.Serial("/dev/ttyACM0")
 
 class colorGUI:
     def __init__(self, master):
@@ -51,7 +51,7 @@ class colorGUI:
         self.bEntry = Entry(self.frame, textvariable=self.bInt)
         self.lengthEntry = Entry(self.frame, textvariable=self.lengthInt)
 
-        self.colorApply = Button(self.frame, text="Apply", command= lambda: self.send())
+        self.apply = Button(self.frame, text="Apply", command= lambda: self.send())
 
         self.rLabel = Label(self.frame, text="R")
         self.gLabel = Label(self.frame, text="G")
@@ -63,34 +63,40 @@ class colorGUI:
         self.bLabel.grid(row=2, column=0)
         self.lengthLabel.grid(row=3, column=0)
 
-        self.frame.grid(row=0, column=1)
         self.rEntry.grid(row=0, column=1)
         self.gEntry.grid(row=1, column=1)
         self.bEntry.grid(row=2, column=1)
         self.lengthEntry.grid(row=3, column=1)
-        self.colorApply.grid(row=4, column=1)
+        self.apply.grid(row=4, column=1)
     
     def send(self):
-        if(self.rInt.get() != self.rOld):
+        if(self.rInt.get() != self.rOld or oldItem != selectedItem.get() or oldColor != selectedColor.get()):
             rCommand = "<" + "c" + str(selectedItem.get()) + str(selectedColor.get()) + "0" + str(hex(self.rInt.get())[2:].zfill(3)) + ">"
             Serial.write(rCommand.encode())
             self.rOld = self.rInt.get()
-
+            updateOldColor()
+            updateOldItem()
             print("New Value" + rCommand)
-        if(self.gInt.get() != self.gOld):
+        if(self.gInt.get() != self.gOld or oldItem != selectedItem.get() or oldColor != selectedColor.get()):
             gCommand = "<" + "c" + str(selectedItem.get()) + str(selectedColor.get()) + "1" + str(hex(self.gInt.get())[2:].zfill(3)) + ">"
             Serial.write(gCommand.encode())
             self.gOld = self.gInt.get()
+            updateOldColor()
+            updateOldItem()
             print("New Value" + gCommand)
-        if(self.bInt.get() != self.bOld):
+        if(self.bInt.get() != self.bOld or oldItem != selectedItem.get() or oldColor != selectedColor.get()):
             bCommand = "<" + "c" + str(selectedItem.get()) + str(selectedColor.get()) + "2" + str(hex(self.bInt.get())[2:].zfill(3)) + ">"
             Serial.write(bCommand.encode())
             self.bOld = self.bInt.get()
+            updateOldColor()
+            updateOldItem()
             print("New Value" + bCommand)
-        if(self.lengthInt.get() != self.lengthOld):
+        if(self.lengthInt.get() != self.lengthOld or oldItem != selectedItem.get() or oldColor != selectedColor.get()):
             lCommand = "<" + "l" + str(selectedItem.get()) + str(selectedColor.get()) + "0" + str(hex(self.lengthInt.get())[2:].zfill(3)) + ">"
             Serial.write(lCommand.encode())
             self.lengthOld = self.lengthInt.get()
+            updateOldColor()
+            updateOldItem()
             print("New Value" + lCommand)
         
         return
@@ -107,7 +113,6 @@ class selectionGUI:
         self.colorLabel = Label(self.frame, text="Color #")
         self.itemLabel = Label(self.frame, text="Item #")
 
-        self.frame.grid(row=0, column=0)
         self.itemLabel.grid(row=0, column=0)
         self.colorLabel.grid(row=1, column=0)
         self.itemSelect.grid(row=0, column=1)
@@ -145,32 +150,32 @@ class itemGUI:
         self.directionChange = Button(self.frame, text="Change Dir", command= lambda: self.send("d"))
         self.positionApply = Button(self.frame, text="Apply Position", command= lambda: self.send("p"))
 
-        self.frame.grid(row=1, column=0)
 
         self.timeLabel.grid(row=0, column=0)
         self.colorCountLabel.grid(row=1, column=0)
-        self.positionLabel.grid(row=3, column=0)
+        self.positionLabel.grid(row=2, column=0)
 
         self.timeEntry.grid(row=0, column=1)
         self.colorCountEntry.grid(row=1, column=1)
-        self.positionEntry.grid(row=3, column=1)
+        self.positionEntry.grid(row=2, column=1)
 
-        self.apply.grid(row=2, column=1)
-        self.directionChange.grid(row=5, column=1)
+        self.apply.grid(row=3, column=0)
+        self.directionChange.grid(row=3, column=1)
         self.positionApply.grid(row=4, column=1)
-
 
     def send(self, type):
         if(type == "a"):
-            if(self.timeInt.get() != self.timeOld):
+            if(self.timeInt.get() != self.timeOld or oldItem != selectedItem.get()):
                 tCommand = "<" + "t" + str(selectedItem.get()) + str(selectedColor.get()) + "0" + str(hex(self.timeInt.get())[2:].zfill(3)) + ">"
                 Serial.write(tCommand.encode())
                 self.timeOld = self.timeInt.get()
+                updateOldItem()
                 print("New Value" + tCommand)
-            if(self.colorCountInt.get() != self.colorCountOld):
+            if(self.colorCountInt.get() != self.colorCountOld or oldItem != selectedItem.get()):
                 cCommand = "<" + "a" + str(selectedItem.get()) + str(selectedColor.get()) + "0" + str(hex(self.colorCountInt.get())[2:].zfill(3)) + ">"
                 Serial.write(cCommand.encode())
                 self.colorCountOld = self.colorCountInt.get()
+                updateOldItem()
                 print("New Value" + cCommand)
         if(type == "d"):
             newDir = 0
@@ -197,26 +202,29 @@ class stripGUI:
         self.itemCountInt = IntVar()
         self.itemCountInt.set(1)
 
+        self.activeOld = 1
+        
         self.itemCountOld = self.itemCountInt.get()
 
         self.itemCountLabel = Label(self.frame, text="Item Count")
-
         self.itemCountEntry = Entry(self.frame, textvariable=self.itemCountInt)
         
+        self.activeButton = Button(self.frame, text="Start/Stop")
         self.apply = Button(self.frame, text="Apply", command= lambda: self.send())
 
-        self.frame.grid(row=1, column=1)
 
+        self.itemCountLabel.grid(row=0, column=0)
         self.itemCountEntry.grid(row=0, column=1)
         self.apply.grid(row=1, column=1)
 
 
 
     def send(self):
-        iCommand = "<" + "i" + str(selectedItem.get()) + str(selectedColor.get()) + "0" + str(hex(self.itemCountInt.get())[2:].zfill(3)) + ">"
-        Serial.write(iCommand.encode())
-        self.itemCountOld = self.itemCountInt.get()
-        print("New Value" + iCommand)
+        if(self.itemCountInt.get() != self.itemCountOld):
+            iCommand = "<" + "i" + str(selectedItem.get()) + str(selectedColor.get()) + "0" + str(hex(self.itemCountInt.get())[2:].zfill(3)) + ">"
+            Serial.write(iCommand.encode())
+            self.itemCountOld = self.itemCountInt.get()
+            print("New Value" + iCommand)
 
 
 
@@ -225,63 +233,18 @@ class mainGUI:
         self.master = master
 
         self.frame = Frame(master)
-        self.frame.pack(side=TOP)
+        self.frame.pack()
 
         self.cGUI = colorGUI(self.frame)
         self.selGUI = selectionGUI(self.frame)
         self.iGUI = itemGUI(self.frame)
-
         self.sGUI = stripGUI(self.frame)
 
-        # self.timeString = IntVar()
-        # self.itemCountString = IntVar()
-        # self.colorCountString = IntVar()
-        # self.positionString = IntVar()
 
-        # self.timeString.set(10)
-        # self.itemCountString.set(1)
-        # self.colorCountString.set(1)
-        # self.positionString.set(0)
-
-        # self.selectionFrame = Frame(self.frame)
-
-
-
-        # self.toggleFrame = Frame(self.frame)
-        # self.start = Button(self.toggleFrame, text="Start")
-        # self.stop = Button(self.toggleFrame, text="Stop")
-        # self.forward = Button(self.toggleFrame, text="Forward")
-        # self.backward = Button(self.toggleFrame, text="Backward")
-
-        # self.toggleFrame.grid(row=2, column=0)
-        # self.start.grid(row=0, column=0)
-        # self.stop.grid(row=0, column=1)
-        # self.forward.grid(row=1, column=0)
-        # self.backward.grid(row=1, column=1)
-
-
-        # self.countFrame = Frame(self.frame)
-        # self.itemCountEntry = Entry(self.countFrame, textvariable=self.itemCountString)
-        # self.colorCountEntry = Entry(self.countFrame, textvariable=self.colorCountString)
-        # self.itemCountButton = Button(self.countFrame, text="Apply")
-        # self.colorCountButton = Button(self.countFrame, text="Apply")
-        # self.colorCountLabel = Label(self.countFrame, text="Color Count")
-        # self.itemCountLabel = Label(self.countFrame, text="Item Count")
-
-        # self.countFrame.grid(row=3, column=0)
-        # self.itemCountLabel.grid(row=0, column=0)
-        # self.itemCountEntry.grid(row=0, column=1)
-        # self.itemCountButton.grid(row=0, column=2)
-
-        # self.colorCountLabel.grid(row=1, column=0)
-        # self.colorCountEntry.grid(row=1, column=1)
-        # self.colorCountButton.grid(row=1, column=2)
-
-        # self.miscFrame = Frame(self.frame)
-        # self.timeEntry = Entry(self.miscFrame, textvariable=self.timeString)
-        # self.positionEntry = Entry(self.miscFrame, textvariable=self.positionString)
-        # self.timeButton = Button(self.miscFrame, text="Set Time")
-        # self.positionButton = Button(self.miscFrame, text="Set Position")
+        self.selGUI.frame.grid(row=1, column=0)
+        self.cGUI.frame.grid(row=0, column=0)
+        self.iGUI.frame.grid(row=0, column=1)
+        self.sGUI.frame.grid(row=1, column=1)
 
 
 
@@ -293,6 +256,15 @@ selectedColor = IntVar()
 
 selectedColor.set(0)
 selectedItem.set(0)
+
+oldItem = 0
+oldColor = 0
+
+def updateOldColor():
+    oldColor = selectedColor.get()
+
+def updateOldItem():
+    oldItem = selectedItem.get()
 
 gui = mainGUI(root)
 
